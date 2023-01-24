b4c45ecabd08de4f839baa79750a3d61d77999d3
Zeyad_Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-01-24 07:06:33 +0200

    added a UI. Added a background image

--- javascript/src.js
+++ javascript/src.js
@@ -12,43 +12,64 @@ function getComputerChoice(){
         return "scissors";
     
 }
+let round = `No rounds yet!`;
+let i = 0
+let playerWins = 0;
+let tie = 0;
+function oneRound(e){
+   // if(i >= 5) return;
 
-function oneRound(playerSelection){
+    console.log(e.target.id);
     let computerChoice = getComputerChoice();
 
-    if(playerSelection.toLowerCase() === computerChoice)
-        return `Tie! ${playerSelection} is as powerful as a ${computerChoice}`;
+    if(e.target.id.toLowerCase() === computerChoice)
+        round = `Tie! ${e.target.id} is as powerful as a ${computerChoice}`;
 
-    else if(playerSelection.toLowerCase() == "rock" && computerChoice=="scissors")
-        return `You Win! ${playerSelection.toLowerCase()} beats ${computerChoice}`;
+    else if(e.target.id.toLowerCase() == "rock" && computerChoice=="scissors")
+        round = `You Win! ${e.target.id.toLowerCase()} beats ${computerChoice}`;
 
-    else if(playerSelection.toLowerCase() == "rock" && computerChoice=="paper")
-        return `You Lose! ${computerChoice} beats ${playerSelection.toLowerCase()}`;
+    else if(e.target.id.toLowerCase() == "rock" && computerChoice=="paper")
+        round = `You Lose! ${computerChoice} beats ${e.target.id.toLowerCase()}`;
 
-    else if(playerSelection.toLowerCase() == "paper" && computerChoice=="rock")
-        return `You Win! ${playerSelection.toLowerCase()} beats ${computerChoice}`;
+    else if(e.target.id.toLowerCase() == "paper" && computerChoice=="rock")
+        round = `You Win! ${e.target.id.toLowerCase()} beats ${computerChoice}`;
 
-    else if(playerSelection.toLowerCase() == "paper" && computerChoice=="scissors")
-        return `You Lose! ${computerChoice} beats ${playerSelection.toLowerCase()}`;
+    else if(e.target.id.toLowerCase() == "paper" && computerChoice=="scissors")
+        round = `You Lose! ${computerChoice} beats ${e.target.id.toLowerCase()}`;
     
-    else if(playerSelection.toLowerCase() == "scissors" && computerChoice=="paper")
-        return `You Win! ${playerSelection.toLowerCase()} beats ${computerChoice}`;
+    else if(e.target.id.toLowerCase() == "scissors" && computerChoice=="paper")
+        round = `You Win! ${e.target.id.toLowerCase()} beats ${computerChoice}`;
 
-    else if(playerSelection.toLowerCase() == "scissors" && computerChoice=="rock")
-        return `You Lose! ${computerChoice} beats ${playerSelection.toLowerCase()}`;
-}
+    else if(e.target.id.toLowerCase() == "scissors" && computerChoice=="rock")
+        round = `You Lose! ${computerChoice} beats ${e.target.id.toLowerCase()}`;
 
-function game(){
-    let playerWins = 0;
-    let tie = 0;
-    for(let i = 0; i <5; i++){
-        let round = oneRound(playerSelection = prompt());
-        console.log(round);
+
+    
+    i++;
+    if(i < 5){
         if(round.indexOf("You Win!") != -1)
             playerWins++;
         else if(round.indexOf("Tie!") != -1)
             tie++;
     }
-    console.log(`Player won ${playerWins} times.`)
-    console.log(`Computer won ${5-tie-playerWins} times.`)
+    if (i >= 5){
+        const playerWon = document.createElement('div');
+        playerWon.style.color= '#0F1F0F';
+        const computerWon = document.createElement('div');
+        computerWon.style.color='#0F1F0F'
+        playerWon.textContent = `Player won ${playerWins} times.`;
+        computerWon.textContent = `Computer won ${5-tie-playerWins} times.`;
+        document.querySelector('body').append(playerWon);
+        document.querySelector('body').append(computerWon);
+        i=0;
+        tie=0
+        playerWins=0;
+    }
 }
+
+
+
+
+
+buts = document.querySelectorAll('button');
+buts.forEach(but => but.addEventListener('click', oneRound));
